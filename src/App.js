30f1f7ffe0e50777dff60a494eba1318cf1a728a
shadid12 <shadidhaquee2012@gmail.com>
2022-02-13 14:36:26 -0500

    added tail wind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link, Outlet, useLocation } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+function Layout() {
+
+  const location = useLocation();
+
+  const selected = `bg-purple-800 text-white px-3 py-2 rounded-md text-sm font-medium`;
+  const unselected = `text-gray-300 px-3 py-2 rounded-md text-black text-sm font-medium`;
+
+  return (
+    <div>
+      <nav className="p-4">
+        <ul className="flex max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Link to="/">
+            <div className="mx-4 flex-shrink-0 flex items-center">
+              <img className="lg:block h-8 w-auto" src="https://images.ctfassets.net/po4qc9xpmpuh/7itYmeRxmVGIXwwGWHrQU3/e4ea73c2bebc64bd65d84964576515b9/fauna-logo-new-v2.svg" alt="Fauna Inc" />
+            </div>
+          </Link>
+          <div className="md:flex md:items-center md:space-x-2">
+            <li className={location.pathname === '/' ? selected : unselected}>
+              <Link to="/">Home</Link>
+            </li>
+            <li className={location.pathname === '/dashboard' ? selected : unselected}>
+              <Link to="/dashboard">Admin Dashboard</Link>
+            </li>
+            <li className={location.pathname === '/about' ? selected : unselected}>
+              <Link to="/about">Docs</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
